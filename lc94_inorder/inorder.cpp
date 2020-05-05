ce0f89f78e23746d32dc6076b310443c5685f4b6
DHacky <dhgacky@gmail.com>
2020-05-05 17:20:18 +0800

    lc94

---
+++ lc94_inorder/inorder.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************
+Copyright (c) 2020 Hacky DH
+
+This file is distributed under MIT License.
+See the LICENSE file for details.
+*******************************************************************/
+
+/*
+94. Binary Tree Inorder Traversal
+
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+
+https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+static bool init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return true;
+}();
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> ans;
+        while(!st.empty() || root != nullptr) {
+            while(root != nullptr) {
+                st.push(root);
+                root = root->left;
+            }
+            root = st.top();
+            st.pop();
+            ans.push_back(root->val);
+            root = root->right;
+        }
+        return ans;
+    }
+};
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
