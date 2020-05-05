bdb9725584f42d62ce73df99dc52cecfb7082198
DHacky <dhgacky@gmail.com>
2020-05-05 23:54:06 +0800

    lc510

--- lc285_inorderSuccessorBST/successor.cpp
+++ lc285_inorderSuccessorBST/successor.cpp
@@ -16,6 +16,8 @@ The successor of a node p is the node with the smallest key greater than p.val.
 Note:
 If the given node has no in-order successor in the tree, return null.
 It's guaranteed that the values of the tree are unique.
+
+https://leetcode.com/problems/inorder-successor-in-bst
 */
 
 #include <iostream>
@@ -33,13 +35,30 @@ class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
         while(root) {
-        	TreeNode* ans = nullptr;
-        	if(root->val > p->val) {
-        		ans = root;
-        		root = root->left;
-        	} else root = root->right;
+            TreeNode* ans = nullptr;
+            if(root->val > p->val) {
+                ans = root;
+                root = root->left;
+            } else root = root->right;
         }
         return ans;
+    }
+
+    // 510. Inorder Successor in BST II
+    // not given root, but has parent pointer
+    Node* inorderSuccessor2(Node* node) {
+        if(node == nullptr) return nullptr;
+        if(node->right) {
+            node = node->right;
+            while(node && node->left) node = node->left;
+            return node;
+        }
+        while(node) {
+            if(node->parent == nullptr) return nullptr;
+            if(node == node->parent->left) return node->parent;
+            node = node->parent;
+        }
+        return node;
     }
 };
 
